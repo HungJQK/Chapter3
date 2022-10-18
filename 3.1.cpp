bdaca0137078bfbf7f2f99f96a36b5f5aeaf5cbe
HungJQK <116071042+HungJQK@users.noreply.github.com>
2022-10-18 16:26:04 +0700

    Add files via upload

---
+++ 3.1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    char ch;
+    cout <<"Enter a charater: ";
+    cin >> ch;
+    cout <<"Its ASCII value: " <<int(ch);
+    //Another way: cout <<"Its ASCII value: " <<int(ch);
+
+    return 0;
+}
